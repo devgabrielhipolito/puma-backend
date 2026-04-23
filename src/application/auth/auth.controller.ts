d1d29da7c6012cad59e687a532ece6433c4c5673
Gabriel <dev.gabrielhipolito@gmail.com>
2026-04-23 20:57:08 -0300

    sa

--- src/application/auth/auth.controller.ts
+++ src/application/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { LoginUserDto } from './dto/login-user.dto';
 import { AuthService } from './auth.service';
 
@@ -6,6 +6,7 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @HttpCode(200)
   @Post()
   async login(@Body() payload: LoginUserDto) {
     return await this.authService.login(payload);
